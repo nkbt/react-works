431da44a681b68d1182eafb62681f73beef01c47
Nik Butenko <nik@butenko.me>
2015-10-24 23:02:35 +1100

    Adapt E2E test for react-swap

---
+++ test-e2e/ReactSwap.js
@@ -0,0 +1,16 @@
+module.exports = {
+  'Smoketest, page with examples should load': function (browser) {
+    browser
+      .url('http://localhost:8080/')
+      .waitForElementVisible('body', 1000)
+      .assert.containsText('body', 'Clickable')
+      .assert.containsText('body', 'Hoverable')
+      .assert.containsText('body', 'Hoverable with delay')
+      .assert.containsText('body', 'Deep Swap')
+      .assert.containsText('body', 'Table Swap')
+      .assert.containsText('body', 'Deep Table Swap')
+      .assert.containsText('body', 'With callback')
+      .assert.containsText('body', 'Toggle from outside')
+      .end();
+  }
+};
