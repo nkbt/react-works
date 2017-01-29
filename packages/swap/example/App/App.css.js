32e947eb732c7255d2692ceb30b2f2bc2d16ca07
Nik Butenko <nik@butenko.me>
2017-01-29 12:09:08 +1100

    Rename example files and remove css-modules

---
+++ packages/swap/example/App/App.css.js
@@ -0,0 +1,25 @@
+export const app = {
+  padding: `2em`
+};
+
+
+const onoff = {
+  width: `100px`,
+  height: `100px`,
+  color: `#fff`,
+  lineHeight: `100px`,
+  textAlign: `center`
+};
+
+
+export const on = {
+  ...onoff,
+  borderRadius: `50px`,
+  backgroundColor: `#c00`
+};
+
+
+export const off = {
+  ...onoff,
+  backgroundColor: `#06c`
+};
--- packages/swap/example/index.js
+++ packages/swap/example/index.js
@@ -2,8 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-const appRoot = document.createElement('div');
+const appRoot = document.createElement(`div`);
 
-appRoot.id = 'app';
+appRoot.id = `app`;
 document.body.appendChild(appRoot);
 ReactDOM.render(<App />, appRoot);
