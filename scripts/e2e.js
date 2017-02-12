661c76386c4b7a6d01063c6cb201286b2ac60ebf
Nik Butenko <nik@butenko.me>
2017-02-12 13:36:03 +1100

    Merge pull request #5 from nkbt/Upgrade-to-Webpack2
    Upgrade to Webpack2

--- scripts/e2e.js
+++ scripts/e2e.js
@@ -1,17 +1,20 @@
 #!/usr/bin/env node
 
 
+const nightwatchAutorun = require(`nightwatch-autorun`);
+const webpack = require(`webpack`);
+const WebpackDevServer = require(`webpack-dev-server`);
+
+
 const {npm, CWD} = require(`../lib/bash`);
 const {output_folder: REPORT_DIR} = require(`../lib/nightwatch.json`);
+const webpackConfig = require(`../lib/webpack/dev.config.js`);
 
 
 npm(`rimraf reports/e2e`, {cwd: CWD});
-npm(`nightwatch-autorun `, {
-  cwd: CWD,
-  env: {
-    NIGHTWATCH_CONFIG: require.resolve(`../lib/nightwatch.json`),
-    WEBPACK_CONFIG: require.resolve(`../lib/webpack/dev.config.js`),
-    REPORT_DIR,
-    LOG_DIR: REPORT_DIR
-  }
+nightwatchAutorun({
+  server: new WebpackDevServer(webpack(webpackConfig)),
+  nightwatchConfig: require.resolve(`../lib/nightwatch.json`),
+  reportDir: REPORT_DIR,
+  logDir: REPORT_DIR
 });
